4a8036fef00812070b9c7f944b279800ea7a21c0
Han Hua <ahua@groupon.com>
2014-05-18 18:56:22 -0500

    Initial Commit

---
+++ HouseWork/HouseWorkAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HouseWorkAppDelegate.h
+//  HouseWork
+//
+//  Created by Alan Hua on 5/18/14.
+//  Copyright (c) 2014 EngineLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HouseWorkAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HouseWork/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HouseWork
+//
+//  Created by Alan Hua on 5/18/14.
+//  Copyright (c) 2014 EngineLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HouseWorkAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HouseWorkAppDelegate class]));
+    }
+}
